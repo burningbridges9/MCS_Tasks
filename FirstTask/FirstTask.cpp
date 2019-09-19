b8895b4a3c2ca2867e4ed3a556009e7678485ae4
burningbridges9 <talipov9@gmail.com>
2019-09-19 21:59:10 +0300

    FirstTask implemented

---
+++ FirstTask/FirstTask.cpp
@@ -0,0 +1,28 @@
+// FirstTask.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "mpi.h"
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <random>
+using namespace std;
+
+
+int main()
+{
+	int size, rank, recv;
+	MPI_Status st;
+
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	printf("Hello from process %d\nNumber of all processes is %d\n\n", rank, size);
+		
+
+	MPI_Finalize();
+    return 0;
+}
+
